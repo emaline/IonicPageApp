57b4bb1b1b5c2a719dc21e4b097b50439279e8d9
Emaline <38117483+emaline@users.noreply.github.com>
2020-02-22 19:52:46 -0800

    Add files via upload

---
+++ IonicPageApp/src/app/chapter2/chapter2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '../home/node_modules/@angular/core';
+
+@Component({
+  selector: 'app-chapter2',
+  templateUrl: './chapter2.page.html',
+  styleUrls: ['./chapter2.page.scss'],
+})
+export class Chapter2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
